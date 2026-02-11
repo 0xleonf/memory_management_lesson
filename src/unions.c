25c9c504bdd5eb0d4ef3ae44cad3615c0e8a8d33
0xleonf <ucihasaskee07@gmail.com>
2026-02-11 17:42:24 +0800

    feat: intial unions

---
+++ src/unions.c
@@ -0,0 +1,22 @@
+#include "unions.h"
+#include <stdio.h>
+
+void format_object(snek_object_t obj, char *buffer) {
+  switch (obj.kind) {
+  case INTEGER:
+    sprintf(buffer, "int:%d", obj.data.v_int);
+    return;
+
+  case STRING:
+    sprintf(buffer, "string:%s", obj.data.v_string);
+    return;
+  }
+}
+
+snek_object_t new_integer(int i) {
+  return (snek_object_t){.kind = INTEGER, .data = {.v_int = i}};
+}
+
+snek_object_t new_string(char *str) {
+  return (snek_object_t){.kind = STRING, .data = {.v_string = str}};
+}
